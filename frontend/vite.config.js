58bb9876f43c3aa64b7bbe84de7b03b5b8a99a61
Anoop Patel <anooppatel582@gmail.com>
2026-01-31 21:53:50 +0530

    updating configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yantrika-nitsikkim/'
+  base: '/yantrika-nitsikkim/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
 })
