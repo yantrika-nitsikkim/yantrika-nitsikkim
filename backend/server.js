aa4799a20dbd30158a10d010ba3cecfe1365d92a
Anoop Patel <anooppatel582@gmail.com>
2026-01-31 21:59:23 +0530

    adding .gitignore

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(flash());
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // your frontend URL
+    origin: 'http://localhost:5173', // frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
 }));
